c52d2cd1bd51875f78f771642ae15ee7a48fd34e
Pavel <zimin.pl22@gmail.com>
2021-04-26 00:55:05 +0400

    fix

--- TreeFactoryBusinessLogic/ViewModels/ReportOrdersViewModel.cs
+++ TreeFactoryBusinessLogic/ViewModels/ReportOrdersViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using TreeFactoryBusinessLogic.Enums;
 
@@ -7,10 +8,15 @@ namespace TreeFactoryBusinessLogic.ViewModels
 {
     public class ReportOrdersViewModel
     {
+        [DisplayName("Дата создания")]
         public DateTime DateCreate { get; set; }
+        [DisplayName("Имя изделия")]
         public string WoodName { get; set; }
+        [DisplayName("Количество")]
         public int Count { get; set; }
+        [DisplayName("Сумма")]
         public decimal Sum { get; set; }
+        [DisplayName("Статус")]
         public OrderStatus Status { get; set; }
     }
 }
